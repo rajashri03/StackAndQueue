7ec38e02486d36cebf1dd4ba5fd6648148d8ed24
rajashri03 <telvekar.rajashri@gmail.com>
2021-12-13 20:10:44 +0530

    Queue using linked list

--- StackAndQueue/StackAndQueue/Program.cs
+++ StackAndQueue/StackAndQueue/Program.cs
@@ -1,13 +1,39 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using StackAndQueue;
-
-Stack_Ex<int> stackdata = new Stack_Ex<int>();
-stackdata.Push(70);
-stackdata.Push(30);
-stackdata.Push(56);
-stackdata.Display();
-stackdata.Peek();
-stackdata.Pop();
-stackdata.Display();
-stackdata.Peek();
-stackdata.Display();
+bool flag = true;
+while (flag)
+{
+    Console.WriteLine("Select:\n1)Stack Using Linked List\n2)Queue using linked list\n");
+    int op = Convert.ToInt16(Console.ReadLine());
+    switch (op)
+    {
+        case 1:
+            Stack_Ex<int> stackdata = new Stack_Ex<int>();
+            stackdata.Push(70);
+            stackdata.Push(30);
+            stackdata.Push(56);
+            stackdata.Display();
+            stackdata.Peek();
+            stackdata.Pop();
+            stackdata.Display();
+            stackdata.Peek();
+            stackdata.Display();
+            break;
+        case 2:
+            Queue_Ex<int> queudata = new Queue_Ex<int>();
+            queudata.Enqueue(70);
+            queudata.Enqueue(30);
+            queudata.Enqueue(56);
+            queudata.Display();
+            queudata.Dequeue();
+            queudata.Display();
+            queudata.Dequeue();
+            queudata.Display();
+            queudata.Dequeue();
+            queudata.Display();
+            break;
+        default:
+            flag = !flag;
+            break;
+    }
+}
---
+++ StackAndQueue/StackAndQueue/Queue_Ex.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackAndQueue
+{
+    public class Queue_Ex<T>
+    {
+        Node<T> head = null;
+        public void Enqueue(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (head == null)
+                head = node;
+            else
+            {
+                Node<T> temp = head;
+                while (temp.Next != null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = node;
+            }
+            Console.WriteLine(node.data + " is Added in Queue");
+        }
+        public Node<T> Dequeue()
+        {
+            if (head == null)
+            {
+                return null;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nAfter Dequeue");
+                Console.ResetColor();
+                head = head.Next;
+                return head;
+            }
+        }
+        public void Display()
+        {
+            Node<T> temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\nList Contains:");
+            Console.ResetColor();
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.Next;
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
